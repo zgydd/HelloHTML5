41eb0d0d694ac79e202853b4e2efe6e2b1063426
bartonjoe <bartonjoe@bartonjoe-VirtualBox>
2017-03-21 17:34:29 +0800

    how to do without heatmap

--- hotPoint/public/js/main.js
+++ hotPoint/public/js/main.js
@@ -1,12 +1,139 @@
+'use strict';
 window.onload = function () {
-    
-    var innerData = getInnerData();
-    var width = innerData.length * 10;
-    var height = innerData[0].length * 10;
+
+    var radius = 20;
+
+    var dataResult = getInnerData();
+    var width = dataResult.data.length * radius + radius;
+    var height = dataResult.data[0].length * radius + radius;
+
+
+    var canvas = null;
+    var context = null;
+    canvas = document.getElementById('canvas');
+
+    canvas.width = width;
+    canvas.height = height;
+    context = canvas.getContext('2d');
+    var denominator = dataResult.max - dataResult.min;
+
+    var gradientDictionary = [{alpha: 52, colorR: 0, colorG: 0, colorB: 255}
+        , {alpha: 104, colorR: 0, colorG: 255, colorB: 0}
+        , {alpha: 153, colorR: 255, colorG: 255, colorB: 0}
+        , {alpha: 205, colorR: 255, colorG: 0, colorB: 0}];
+
+    for (var i = 0; i < dataResult.data.length; i++) {
+        for (var j = 0; j < dataResult.data[i].length; j++) {
+            if (dataResult.data[i][j] === 0)
+                continue;
+            var alp = (dataResult.data[i][j] - dataResult.min) / denominator;
+            var gradient = context.createRadialGradient(i * radius + radius, j * radius + radius, 0, i * radius + radius, j * radius + radius, radius);
+            gradient.addColorStop(0, 'rgba(0,0,0,' + alp + ')');
+            gradient.addColorStop(1, 'rgba(0,0,0,0)');
+            context.fillStyle = gradient;
+            context.fillRect(i * radius, j * radius, radius * 2, radius * 2);
+        }
+    }
+
+    var imgData = context.getImageData(0, 0, width, height);
+    //    console.log(imgData.data);
+    for (var i = 0; i < imgData.data.length; i += 4) {
+//        for (var j = gradientDictionary.length - 1; j >= 0; j--) {
+//            if (imgData.data[i + 3] > gradientDictionary[j].alpha) {
+//                imgData.data[i] = gradientDictionary[j].colorR;
+//                imgData.data[i + 1] = gradientDictionary[j].colorG;
+//                imgData.data[i + 2] = gradientDictionary[j].colorB;
+//                break;
+//            }
+//        }
+        if (imgData.data[i + 3] <= 128)
+            imgData.data[i] = imgData.data[i + 3] * 2;
+        else
+            imgData.data[i] = 256;
+
+
+        if (imgData.data[i + 3] < 85)
+            imgData.data[i] = imgData.data[i + 3] * 3;
+        else if (imgData.data[i + 3] < 170)
+            imgData.data[i + 1] = 256;
+        else
+            imgData.data[i + 1] = 256 - imgData.data[i + 3] * 3;
+
+        imgData.data[i + 2] = 256 - imgData.data[i + 3];
+    }
+    context.putImageData(imgData, 0, 0);
+
+
+
+    canvas = document.getElementById('canvas2');
+
+    canvas.width = width;
+    canvas.height = height;
+    context = canvas.getContext('2d');
+    var denominator = dataResult.max - dataResult.min;
+
+    var gradientDictionary = [{alpha: 52, colorR: 0, colorG: 0, colorB: 255}
+        , {alpha: 104, colorR: 0, colorG: 255, colorB: 0}
+        , {alpha: 153, colorR: 255, colorG: 255, colorB: 0}
+        , {alpha: 205, colorR: 255, colorG: 0, colorB: 0}];
+
+    for (var i = 0; i < dataResult.data.length; i++) {
+        for (var j = 0; j < dataResult.data[i].length; j++) {
+            if (dataResult.data[i][j] === 0)
+                continue;
+            var alp = (dataResult.data[i][j] - dataResult.min) / denominator;
+
+            var gradientIdx = 0;
+            for (gradientIdx = 0; gradientIdx < gradientDictionary.length; gradientIdx++) {
+                if (alp > gradientDictionary[gradientIdx].alpha)
+                    break;
+            }
+
+            if (gradientIdx <= 0)
+                break;
+            context.beginPath();
+            context.arc(i * radius + radius, j * radius + radius, radius, 0, Math.PI * 2);
+            context.closePath();
+
+            var gradient = context.createRadialGradient(i * radius + radius, j * radius + radius, 0, i * radius + radius, j * radius + radius, radius);
+            gradient.addColorStop(0, 'rgba(0,0,0,' + alp + ')');
+            gradient.addColorStop(1, 'rgba(0,0,0,0)');
+            context.fillStyle = gradient;
+            context.fill();
+        }
+    }
+
+    var imgData = context.getImageData(0, 0, width, height);
+    //    console.log(imgData.data);
+    for (var i = 0; i < imgData.data.length; i += 4) {
+        for (var j = gradientDictionary.length - 1; j >= 0; j--) {
+            if (imgData.data[i + 3] > gradientDictionary[j].alpha) {
+                imgData.data[i] = gradientDictionary[j].colorR;
+                imgData.data[i + 1] = gradientDictionary[j].colorG;
+                imgData.data[i + 2] = gradientDictionary[j].colorB;
+                break;
+            }
+        }
+//        if (imgData.data[i + 3] <= 128)
+//            imgData.data[i] = imgData.data[i + 3] * 2;
+//        else
+//            imgData.data[i] = 256;
+//
+//
+//        if (imgData.data[i + 3] < 85)
+//            imgData.data[i] = imgData.data[i + 3] * 3;
+//        else if (imgData.data[i + 3] < 170)
+//            imgData.data[i + 1] = 256;
+//        else
+//            imgData.data[i + 1] = 256 - imgData.data[i + 3] * 3;
+//
+//        imgData.data[i + 2] = 256 - imgData.data[i + 3];
+    }
+    context.putImageData(imgData, 0, 0);
+
 
     $('.heatmap').width(width);
     $('.heatmap').height(height);
-
     // minimal heatmap instance configuration
     var heatmapInstance = heatmap.create({
         // only container is required, the rest will be defaults
@@ -16,15 +143,15 @@ window.onload = function () {
     // now generate some random data
     var points = [];
     var max = 0;
-    for (var i = 0; i < innerData.length; i++) {
-        for (var j = 0; j < innerData[i].length; j++) {
-            if (innerData[i][j] === 0)
+    for (var i = 0; i < dataResult.data.length; i++) {
+        for (var j = 0; j < dataResult.data[i].length; j++) {
+            if (dataResult.data[i][j] === 0)
                 continue;
-            max = Math.max(max, innerData[i][j]);
+            max = Math.max(max, dataResult.data[i][j]);
             var point = {
-                x: i * 10,
-                y: j * 10,
-                value: innerData[i][j]
+                x: i * radius,
+                y: j * radius,
+                value: dataResult.data[i][j]
             };
             points.push(point);
         }
@@ -41,70 +168,40 @@ window.onload = function () {
 };
 
 var getInnerData = function () {
-//    var innerData = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//                , [0, 300, 600, 600, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 800, 0]
-//                , [0, 200, 700, 600, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//                , [0, 200, 600, 400, 300, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//                , [0, 100, 0, 0, 0, 0, 300, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 200, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 200, 100, 0, 0, 0, 0, 0, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 100, 50, 0, 0, 0, 0, 0, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 100, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 600, 650, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 600, 700, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 100, 500, 800, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 100, 200, 200, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 100, 0, 0, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 0, 0, 0, 0, 0]
-//                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-    var innerData = [];
-    for (var j = 0; j < 32; j++) {
-        var inLine = [];
-        for (var i = 0; i < 80; i++) {
-            var tmpRad = Math.round(Math.random() * 1000);
-            if (tmpRad < 600)
-                tmpRad = 0;
-            if (tmpRad > 700)
-                tmpRad = 0;
-            inLine.push(tmpRad);
-        }
-        innerData.push(inLine);
-    }
-    console.log(innerData);
-    return innerData;
+    var result = {max: 800, min: 0, data: []};
+    var innerData = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                , [0, 300, 600, 600, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 800, 0]
+                , [0, 200, 700, 600, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                , [0, 200, 600, 400, 300, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                , [0, 100, 0, 0, 0, 0, 300, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 200, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 200, 100, 0, 0, 0, 0, 0, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 100, 50, 0, 0, 0, 0, 0, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 100, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 600, 650, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 600, 700, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 100, 500, 800, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 100, 200, 200, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 100, 0, 0, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 0, 0, 0, 0, 0]
+                , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    result.data = innerData;
+//    var result = {max: 0, min: 1000, data: []};
+//    for (var j = 0; j < 32; j++) {
+//        var inLine = [];
+//        for (var i = 0; i < 80; i++) {
+//            var tmpRad = Math.round(Math.random() * 1000);
+//            if (result.max < tmpRad)
+//                result.max = tmpRad;
+//            if (result.min > tmpRad)
+//                result.min = tmpRad;
+//
+//            if (tmpRad > 700 || tmpRad < 600)
+//                tmpRad = 0;
+//
+//            inLine.push(tmpRad);
+//        }
+//        result.data.push(inLine);
+//    }
+    return result;
 };
-/*
- var canvas = null;
- var context = null;
- window.onload = function () {
- 
- getInnerData();
- canvas = document.getElementById('canvas');
- //    canvas.onclick = function(e){
- //        console.log(e);
- //    }
- canvas.width = innerData.length * 20 + 20;
- canvas.height = innerData[0].length * 20 + 20;
- context = canvas.getContext('2d');
- 
- for (var i = 0; i < innerData.length; i++) {
- for (var j = 0; j < innerData[i].length; j++) {
- //            if (innerData[i][j] < 480)
- //                continue;
- var add = 2;
- var alp = innerData[i][j] / 1000;
- var gradient = context.createRadialGradient(i * 20 + (10 * add), j * 20 + (10 * add), (10 * add), i * 20 + (10 * add), j * 20 + (10 * add), (5 * add));
- gradient.addColorStop(1, 'rgba(255,0,0,' + alp + ')');
- gradient.addColorStop(0.8, 'rgba(255,0,0,' + alp/2 + ')');
- gradient.addColorStop(0.6, 'rgba(255,0,0,' + alp/4 + ')');
- gradient.addColorStop(0.4, 'rgba(255,0,0,' + alp/6 + ')');
- gradient.addColorStop(0.2, 'rgba(255,0,0,' + alp/8 + ')');
- gradient.addColorStop(0, 'rgba(255,0,0,0)');
- context.fillStyle = gradient;
- context.fillRect(i * 20, j * 20, 20 * add, 20 * add);
- //context.save();
- }
- }
- };
- 
- */
